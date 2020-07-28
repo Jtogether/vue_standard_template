44fceb9a73fa50f2d7d82606ca65fcf2203fbc7f
J.T. Szeto <Jtogether@163.com>
2020-07-28 14:25:09 +0800

    template,upload

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<!--这里是主页，也是一整个完整外层-->
+	<div class="home">
+
+	</div>
+</template>
+
+<script>
+	
+
+
+	export default {
+		name: 'Home',
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
